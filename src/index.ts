330e1a7f204d2942c1e8f19a5fc5fb0dbf8aa710
Carla Clemente <carlaclefig@gmail.com>
2026-02-25 13:24:08 -0500

    feat: add CORS in the server configuration to allow requests from frontend development servers

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,19 @@ import songsRouter from "./routes/song.routes";
 import { staticServer } from "./staticServer";
 import { connectDB } from "./db/postgres";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
-const port = process.env["DB_PORT"] || 3000;
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  }),
+);
+
+const port = process.env["PORT"] || 3000;
 
 app.use(express.json());
 
